90223a8335b8ce772e18221071688b0cf6b76acf
Tariq Mushtaq <tariq.mush@gmail.com>
2025-06-14 23:36:20 +0000

    Cleanup

--- invoice_ocr/invoice_ocr/doctype/invoice_upload/test_invoice_upload.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2025, mohtashim and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestInvoiceUpload(FrappeTestCase):
-	pass
